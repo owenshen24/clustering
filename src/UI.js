6d514009be6e70f1dbcfdc34ccf7ca7f99e7170d
Owen <owenshen24@gmail.com>
2020-04-22 18:01:46 -0700

    changed update

--- src/UI.js
+++ src/UI.js
@@ -50,8 +50,47 @@ function run_cluster(graph, graph_drawer, axis) {
 function show_sample() {
   let data = {};
   data["directed"] = false;
-  data["nodes"] = [{"id":"1"},{"id":"2"},{"id":"3"}];
-  data["edges"] = [{"source": "1", "target": "2"}, {"source": "2", "target": "3"}, {"source": "1", "target": "3"}];
+  data["nodes"] = [
+    {"id":"1"}, 
+    {"id":"2"}, 
+    {"id":"3"}, 
+    {"id":"4"}, 
+    {"id":"5"}, 
+    {"id":"6"}, 
+    {"id":"7"}, 
+    {"id":"8"}, 
+    {"id":"9"}, 
+    {"id":"10"}, 
+    {"id":"11"}, 
+    {"id":"12"}, 
+    {"id":"13"}, 
+    {"id":"14"}, 
+    {"id":"15"}, 
+    {"id":"16"}, 
+    {"id":"17"}, 
+    {"id":"18"}, 
+    {"id":"19"}
+  ];
+  data["edges"] = [
+    {"source":"1", "target":"2"}, 
+    {"source":"2", "target":"3"}, 
+    {"source":"3", "target":"4"}, 
+    {"source":"4", "target":"5"}, 
+    {"source":"5", "target":"6"}, 
+    {"source":"6", "target":"7"}, 
+    {"source":"7", "target":"8"}, 
+    {"source":"8", "target":"9"}, 
+    {"source":"9", "target":"10"}, 
+    {"source":"10", "target":"11"}, 
+    {"source":"11", "target":"12"}, 
+    {"source":"12", "target":"13"}, 
+    {"source":"13", "target":"14"}, 
+    {"source":"14", "target":"15"}, 
+    {"source":"15", "target":"16"}, 
+    {"source":"16", "target":"17"}, 
+    {"source":"17", "target":"18"}, 
+    {"source":"18", "target":"19"}
+  ];
   init_graph(data);
 }
 
